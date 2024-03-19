3beecab11cff836ce790dc7e2c4a16b397c2d360
Gaetano Loizzo <g.loizzo9@studenti.uniba.it>
2024-03-19 22:42:01 +0100

    Aggiunta Navigazione Stanze e Zoom Oggetti

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public interface IInteractable
+{
+    void Interact(DisplayImage currentDisplay);
+}
+

a20e2c29fcb82799211e0999b10aec0d5775e21b
Gaetano Loizzo <g.loizzo9@studenti.uniba.it>
2024-03-22 04:37:15 +0100

    Aggiunto Script ExitDoor, Messaggio Uscita e Conclusione Gioco

---
+++ Assets/Scripts/ExitDoor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitDoor : MonoBehaviour, IInteractable
+{
+    public GameObject ChangedStateSprite;
+
+    public string UnlockItem;
+
+    public GameObject EscapeMessage;
+
+    private GameObject inventory;
+
+
+    void Start()
+    {
+        ChangedStateSprite.SetActive(false);
+        inventory = GameObject.Find("Inventory");
+    }
+
+
+
+    public void Interact(DisplayImage currentDisplay)
+    {
+        if (inventory.GetComponent<Inventory>().currentSelectedSlot.gameObject.transform.GetChild(0).GetComponent<Image>().sprite.name == UnlockItem || UnlockItem == "")
+        {
+            ChangedStateSprite.SetActive(true);
+
+            this.gameObject.layer = 0;
+
+            Instantiate(EscapeMessage, GameObject.Find("wall7").transform);
+            this.gameObject.layer = 1;
+        }
+
+    }
+}
